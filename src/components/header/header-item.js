6ccbbbd700b7806f4bdadcd89342b41a0a45200b
Paul Banach <pawel.banach@chargify.com>
2019-11-11 15:42:58 +0100

    Clean up

--- src/components/header/header-item.js
+++ src/components/header/header-item.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import {Link} from 'gatsby';
+import styles from './header.module.scss';
 
 const HeaderItem = ({title}) => (
-  <Link style={{
-    fontSize: `12px`,
-    color: `#000000`,
-    letterSpacing: `3.5px`,
-    fontWeiht: 500,
-    textTransform: `uppercase`,
-    padding: `15px`,
-    textDecoration: `none`,
-    opacity: `.5`,
-    transition: `opacity .25s ease-in-out`,
-    alignSelf: `flex-start`,
-    fontFamily: `'Source Sans Pro', sans-serif`,
-    float: `right`,
-    lineHeight: `57px`,
-    // Add hover after adding styled components
-    // ':hover': {
-    //     opacity: 1,
-    //     color: `#000000`,
-    // }
-  }}>
+  <Link className={styles.item}>
     {title}
   </Link>
 );
