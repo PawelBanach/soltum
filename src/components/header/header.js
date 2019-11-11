062785c1b46a4651a56e61e1d3e074b21d81f219
Paul Banach <pawel.banach@chargify.com>
2019-11-11 12:16:08 +0100

    Add component-scoped styles with CSS-modules

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types"
 import React, { useState, useEffect } from "react"
 import Logo from "../logo-components/logo"
-import HeaderItem from "../header/header-item"
+import HeaderItem from "./header-item"
+import styles from "./header.module.scss"
 
 const Header = () => {
   const [navBackground, setNavBackground] = useState(false);
@@ -15,35 +16,12 @@ const Header = () => {
       document.removeEventListener('scroll', handleScroll)
     }
   }, [])
+  console.log(styles)
   return (
-    <header
-      style={{
-        display: `block`,
-        background: `black`,
-      }}
-    >
-      <nav style={{
-            overflow: `hidden`,
-            position: `fixed`,
-            top: 0,
-            width: `100%`,
-            display: `-webkit-box`,
-            display: ` -webkit-flex`,
-            display: `-ms-flexbox`,
-            display: `flex`,
-            // justifyContent: `center`,
-            zIndex: 1,
-            color: `hsla(0,0%,0%,0.7)`,
-            // backgroundColor: `#fff`,
-            backgroundColor: `${navBackground ? "#ffffff" : "transparent"}`,
-            boxShadow: `${navBackground ? "0px 1px 20px -10px rgba(0,0,0,0.75)" : "none"}`,
-            transition: '1s ease',
-            fontFamily: `'Source Sans Pro',sans-serif`,
-      }}>
-        <Logo />
-        <div style={{
-          marginLeft: `auto`,
-        }}>
+    <header className={styles.header}>
+      <nav className={`${navBackground ? styles.whiteBg : styles.transparentBg} ${styles.nav}`}>
+        <Logo className={styles.logo}/>
+        <div className={styles.menu}>
           <HeaderItem title="contact" />
           <HeaderItem title="projects" />
           <HeaderItem title="technologies" />
@@ -51,25 +29,6 @@ const Header = () => {
           <HeaderItem title="story" />
         </div>
       </nav>
-      {/* <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `1.45rem 1.0875rem`,
-        }}
-      >
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="/"
-            style={{
-              color: `white`,
-              textDecoration: `none`,
-            }}
-          >
-            Test
-          </Link>
-        </h1>
-      </div> */}
     </header>
   )
 }
